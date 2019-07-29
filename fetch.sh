1a2b1b2ac351c8f6624484882cf9bbbc70ec7dac
Harpo Roeder <hroeder@launchdarkly.com>
2019-07-29 10:19:18 -0700

    credit download script

--- fetch.sh
+++ fetch.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# script from https://blog.markvincze.com/download-artifacts-from-a-latest-github-release-in-sh-and-powershell/
 set -e
 LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/launchdarkly/roku-client-sdk/releases/latest)
 LATEST_VERSION=$(echo $LATEST_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
