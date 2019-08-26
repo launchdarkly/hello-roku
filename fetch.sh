ed453ec544a9261459e3c0c710efdb0e9628698f
Harpo Roeder <hroeder@launchdarkly.com>
2019-08-26 13:46:52 -0700

    move build to start of makefile so that it is default

--- fetch.sh
+++ fetch.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-# script from https://blog.markvincze.com/download-artifacts-from-a-latest-github-release-in-sh-and-powershell/
 set -e
 curl -L -o package.zip https://github.com/launchdarkly/roku-client-sdk/releases/download/1.0.0-beta.2/package.zip
