4dbda45886eaaa2c0fbbe6b322135e35c255167d
hroederld <hroeder@launchdarkly.com>
2019-10-08 11:05:46 -0700

    bump SDK version (#7)

--- fetch.sh
+++ fetch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -e
-curl -L -o package.zip https://github.com/launchdarkly/roku-client-sdk/releases/download/1.0.0-rc.1/package.zip
+curl -L -o package.zip https://github.com/launchdarkly/roku-client-sdk/releases/download/1.0.0-rc.2/package.zip
